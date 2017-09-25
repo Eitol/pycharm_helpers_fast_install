4e7a6cc88104b48ca5cef57bcdb40219ac2e8eb7
Hector Oliveros <hector@instant.com.ve>
2017-09-25 11:23:26 -0400

    Add readme

---
+++ README.md
@@ -0,0 +1,40 @@
+<p align="center">
+  <h3 align="center">PyCharm Helpers Fast Install</h3>
+
+  <p align="center">
+    The fast way to install The PyCharm Helpers in a remote server
+  </p>
+</p>
+
+<br>
+
+##### The problem
+This script download the whole helper (from github servers) as one package and then extract it (for realy better performance).
+
+
+##### Usage
+To install the helpers on a remote server you must do the following:
+```bash
+git clone https://github.com/Eitol/pycharm_helpers_fast_install.git
+cd pycharm_helpers_fast_install
+bash install_helpers.sh PYCHARM_VERSION
+# EXAMPLE  bash install_helpers.sh 2017.2.3
+```
+
+It will download and install the helpers according to the version of PyCharm that you want.
+
+After this, in the home folder, a directory named .pycharm_helpers with the following structure must be created
+
+```
+/home/user/.pycharm_helpers
+          ├── build.txt
+          ├── check_all_test_suite.py
+          ├── conda_packaging_tool.py
+          ├── coveragepy
+          ├── coverage_runner
+          ├── docstring_formatter.py
+          ├── extra_syspath.py            
+          ....    
+```
+
+<a href="https://github.com/Eitol/pycharm_helpers"><strong>Go to helpers repo&raquo;</strong></a>
--- install_helpers.sh
+++ install_helpers.sh
@@ -1,16 +1,85 @@
 #!/usr/bin/env bash
 
+###### CONFIG #######
 BASE_URL="https://github.com/Eitol/pycharm_helpers/raw/master/"
 EXTENSION=".tar.gz"
-HELPER_REPO_URL=${BASE_URL}$1${EXTENSION}
+HELPER_FILE_NAME=$1${EXTENSION}
+HELPER_REPO_URL=${BASE_URL}${HELPER_FILE_NAME}
 HELPERS_PATH=~/.pycharm_helpers
+HELPERS_LIST_FILE_NAME="VERSIONS.txt"
+HELPERS_LIST="VOID"
+##### END CONFIG ####
 
-# Create the helper path dir if not exist
-if [ ! -e ${HELPERS_PATH} ] ; then
-    mkdir ${HELPERS_PATH}
-fi
+##### MISC #####
+function print_red {
+    echo -e "\e[31m$1"
+}
 
+function progress_filter {
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c
+    do
+        if $flag
+        then
+            printf '%c' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]
+            then
+                count=0
+            else
+                ((count++))
+                if ((count > 1))
+                then
+                    flag=true
+                fi
+            fi
+        fi
+    done
+}
+##### END MISC #####
 
-cd HELPERS_PATH
+##### FUNCTIONS #####
+function print_available_helpers_version {
+    if [ "${HELPERS_LIST}" == "VOID"  ] ; then
+        wget -q ${BASE_URL}${HELPERS_LIST_FILE_NAME}
+        HELPERS_LIST=$( cat ${HELPERS_LIST_FILE_NAME} )
+        rm ./${HELPERS_LIST_FILE_NAME}
+    fi
+    echo "Available PyCharm helper versions:"
+    echo ${HELPERS_LIST}
+}
 
-#wget ${PHELPER_URL}
+function prepare_helper_folder {
+    # Create the helper path dir if not exist
+    if [ ! -e ${HELPERS_PATH} ] ; then
+        mkdir ${HELPERS_PATH}
+    fi
+}
+
+function enter_in_helper_folder {
+    cd ${HELPERS_PATH}
+}
+
+function get_helper_from_repo {
+    # Get the .tar.gz from the repo
+    wget --progress=bar:force ${HELPER_REPO_URL} 2>&1 | progress_filter
+
+    if [ ! -e ${HELPER_FILE_NAME} ] ; then
+        tput setaf 1
+        print_red "ERROR: Helper with version $1 not found in destination repo"
+        exit 1
+    fi
+}
+
+function extract_helper {
+    tar -xzf ./${HELPER_FILE_NAME} --strip 1
+}
+##### END FUNCTIONS #####
+
+####### ROUTINE ########
+prepare_helper_folder
+enter_in_helper_folder
+get_helper_from_repo
+extract_helper
+echo "FINISH :)"
+###### END ROUTINE #####
